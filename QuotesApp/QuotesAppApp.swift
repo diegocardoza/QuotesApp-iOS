99deb8a06c3ab096f17d4d7494378d7e7f0230b9
Diego Cardoza <diegoalfredocar@gmail.com>
2025-01-05 13:51:48 -0600

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Diego Alfredo Cardoza Ojeda on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
